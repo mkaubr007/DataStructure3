d358faf480a780d6552474be7fc493c9fe8fe006
mkaubr007 <mkaubr007@gmail.com>
2021-09-12 16:38:39 +0530

    UnOrderedList added

---
+++ src/main/java/com/biz/datastructure/programs/MyNode.java
@@ -0,0 +1,11 @@
+package com.biz.datastructure.programs;
+
+class MyNode<T>{
+    T data;	//data to be stored
+    MyNode<T> next;	//reference to the next node
+
+    MyNode(T data){
+        this.data = data;
+        next = null;
+    }
+}
